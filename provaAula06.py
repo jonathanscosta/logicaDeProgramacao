b25d2c615ab1089cbf6fa5db2345de293bcf3276
jonathanscosta <costajonathan34@gmail.com>
2024-12-25 07:27:53 -0300

    prova referente a aula 6 do módulo de lógica de programação

---
+++ provaAula06.py
@@ -0,0 +1,19 @@
+login = input("digite seu login  ").lower().strip()
+senha = int(input("digite sua senha de 5 digitos  "))
+loginCorreto = 'jonathan'
+senhaCorreta = 12345
+tentativas = 3
+
+while tentativas >0:
+    if login == loginCorreto and senha == senhaCorreta:
+        print("acesso liberado")
+        break
+    else:
+        tentativas -=1
+        if tentativas>0:
+            print(F"seu login ou senha estão errados você tem {tentativas} tentativas")
+            login = input("digite seu login novamente  ").lower().strip()
+            senha = int(input("digite sua senha novamente  "))
+else:
+    print("acesso negado")
+
